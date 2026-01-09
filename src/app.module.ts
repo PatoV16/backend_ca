0d5f9a60ff2e5917148b43894dcdae117a0e195a
PatoV16 <133371436+PatoV16@users.noreply.github.com>
2026-01-08 23:29:28 -0500

    fix module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,47 @@
+// import { Module } from '@nestjs/common';
+// import { ConfigModule } from '@nestjs/config';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+
+// import { ConfigurationModule } from './config/configuration.module';
+// import { UsersModule } from './users/users.module';
+// import { AuthModule } from './auth/auth.module';
+// import { InventoryModule } from './inventory/inventory.module';
+// import { WorkOrdersModule } from './work-orders/work-orders.module';
+// import { PostsModule } from './posts/posts.module';
+
+// @Module({
+//   imports: [
+//     // 🔹 ENV GLOBAL
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//     }),
+
+//     // 🔹 POSTGRES
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: process.env.DB_HOST,
+//       port: Number(process.env.DB_PORT),
+//       username: process.env.DB_USER,
+//       password: process.env.DB_PASSWORD,
+//       database: process.env.DB_NAME,
+//       autoLoadEntities: true,
+//       synchronize: true, // ⚠️ solo en desarrollo
+//     }),
+
+//     ConfigurationModule,
+
+//     UsersModule,
+
+//     AuthModule,
+
+//     InventoryModule,
+
+//     WorkOrdersModule,
+
+//     PostsModule,
+//   ],
+// })
+// export class AppModule {}
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -11,33 +55,25 @@ import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
-    // 🔹 ENV GLOBAL
     ConfigModule.forRoot({
       isGlobal: true,
     }),
 
-    // 🔹 POSTGRES
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true, // ⚠️ solo en desarrollo
+      synchronize: true, // ⚠️ solo dev
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
 
     ConfigurationModule,
-
     UsersModule,
-
     AuthModule,
-
     InventoryModule,
-
     WorkOrdersModule,
-
     PostsModule,
   ],
 })
